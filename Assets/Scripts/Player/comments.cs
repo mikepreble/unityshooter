49f91ad5fc9c9017bdcfa1774614b84c071a3ae7
mikepreble <mikepreble@gmail.com>
2015-02-15 12:20:05 -0500

    Create comments.cs

---
+++ Assets/Scripts/Player/comments.cs
@@ -0,0 +1,4 @@
+Transform player;               // Reference to the player's x,y,z position.
+PlayerHealth playerHealth;      // player health
+EnemyHealth enemyHealth;        // Reference to the health.
+NavMeshAgent nav;               // Reference to the agent.
